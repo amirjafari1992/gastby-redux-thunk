0c28661c49897c3d61fc6d28352e5848e341a7d6
amir <amir@mindly.ee>
2020-05-04 10:21:12 +0300

    first commit

--- src/actions/sampleAction.js
+++ src/actions/sampleAction.js
@@ -1,7 +1,6 @@
 import { types } from "./types";
 
 export const sampleFunction = (data) => (dispatch) => {
-  alert(data)
       dispatch({
         type: types.SAMPLE_TYPE,
         darkModeStatus: data,
